5bae3ca5c8a3648676518035702695d0d56c24ef
Sasuke1003 <79127951+Sasuke1003@users.noreply.github.com>
2023-03-19 04:36:31 +0800

    1

---
+++ module/javascript/index.js
@@ -0,0 +1,6 @@
+let text;
+//let robotcheck = prompt("Please input..I am not Robot..to prove you are not robot.");
+switch(robotcheck) {
+  case "I am not Robot":
+    alert = "Excellent choice! Coca-Cola is good for your soul.";
+}

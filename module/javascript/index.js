2f7fe2161c6174cfb4c6683e162ad916b9fbd6ac
Sasuke1003 <79127951+Sasuke1003@users.noreply.github.com>
2023-03-19 13:52:53 +0800

    1

--- module/javascript/index.js
+++ module/javascript/index.js
@@ -1,3 +1,10 @@
+
+
+/*THIS THEME NAME CALLED "ITE3714 GroupProject".
+MAKING BY Chung's' Studio.
+OWNER: Lau Kin Chung
+Instagram: @chung.lkc
+OTHER PORTFOLIO WOULD BE FOUND ON "https://github.com/Sasuke1003"*/
 let text;
 //let robotcheck = prompt("Please input..I am not Robot..to prove you are not robot.");
 switch(robotcheck) {

c554e184387dbc915887caa366352c6320d952c5
Sasuke1003 <79127951+Sasuke1003@users.noreply.github.com>
2023-03-20 01:41:56 +0800

    1

--- module/javascript/index.js
+++ module/javascript/index.js
@@ -5,9 +5,3 @@ MAKING BY Chung's' Studio.
 OWNER: Lau Kin Chung
 Instagram: @chung.lkc
 OTHER PORTFOLIO WOULD BE FOUND ON "https://github.com/Sasuke1003"*/
-let text;
-//let robotcheck = prompt("Please input..I am not Robot..to prove you are not robot.");
-switch(robotcheck) {
-  case "I am not Robot":
-    alert = "Excellent choice! Coca-Cola is good for your soul.";
-}

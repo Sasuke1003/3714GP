dba2408cb5ef4108134b67ad1c69d781799b6f6b
Sasuke1003 <79127951+Sasuke1003@users.noreply.github.com>
2023-03-23 02:08:24 +0800

    1

--- zh-hk/module/javascript/index.js
+++
@@ -1,7 +0,0 @@
-
-
-/*THIS THEME NAME CALLED "ITE3714 GroupProject".
-MAKING BY Chung's' Studio.
-OWNER: Lau Kin Chung
-Instagram: @chung.lkc
-OTHER PORTFOLIO WOULD BE FOUND ON "https://github.com/Sasuke1003"*/

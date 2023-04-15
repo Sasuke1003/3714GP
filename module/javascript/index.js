fc7c18aeddabc0acbf092b726f76f4bb85b72394
Sasuke1003 <79127951+Sasuke1003@users.noreply.github.com>
2023-04-16 03:58:00 +0800

    1

--- module/javascript/index.js
+++ module/javascript/index.js
@@ -5,3 +5,15 @@ MAKING BY Chung's' Studio.
 OWNER: Lau Kin Chung
 Instagram: @chung.lkc
 OTHER PORTFOLIO WOULD BE FOUND ON "https://github.com/Sasuke1003"*/
+
+let showingAlert = false;
+const title = 'ITE3714 TEAM B';
+const messages = 'MAKING BY CHUNG'
+
+const changeTitle = setInterval(() => {
+
+    document.title = 
+        showingAlert ? title : messages;
+
+    showingAlert = !showingAlert;
+}, 700);
